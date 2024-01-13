d53b330e6a382161662bb7bce5647bd5fd94b956
KITA-DS12 <kita.ds12@gmail.com>
2024-01-14 01:52:53 +0900

    add index.vue

---
+++ components/TextAnime.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+const props = defineProps({
+  text: {
+    type: Array<string[]>,
+    required: true,
+  },
+});
+const text = props.text;
+</script>
+<template>
+  <div class="TextAnime">
+    <span v-for="(line, i) in text" :key="i">
+      <span v-for="(char, j) in line" :key="j" class="item">
+        {{ char }}
+      </span>
+      <br />
+    </span>
+  </div>
+</template>
+<style scoped>
+.TextAnime {
+  font-weight: bold;
+}
+
+.item {
+  animation: appear 1.5s ease-in-out forwards;
+}
+
+@keyframes appear {
+  0% {
+    opacity: 0;
+    transform: translateY(2rem);
+  }
+
+  100% {
+    opacity: 1;
+    transform: translateY(0);
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,30 @@
 <template>
-  <div>
-    <h1>Home</h1>
+  <div class="container">
+    <div class="spacer" />
+    <div class="text">
+      <TextAnime :text="[`Hello World !`, `I'm KITA Ryota.`]" />
+    </div>
+    <div class="spacer" />
+    <img src="/code-and-music.png" alt="Code and Music with white background" />
   </div>
 </template>
+<style scoped>
+.container {
+  text-align: center;
+  font-family: monospace;
+  color: #333;
+}
+
+.text {
+  font-size: 2rem;
+  font-weight: bold;
+}
+
+.spacer {
+  height: 3rem;
+}
+
+img {
+  width: 60%;
+}
+</style>
