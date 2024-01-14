264cd7b77fc79db6a6c49979ea94766f4172922b
KITA-DS12 <kita.ds12@gmail.com>
2024-01-14 16:54:53 +0900

    rename public/parts -> assets/parts

--- components/Nav.vue
+++ components/Nav.vue
@@ -22,7 +22,7 @@ const isCurrentPage = (path: string) => {
       </div>
       <div class="header_btn_block_wrapper">
         <button class="hamburger" v-if="!activeMenu" @click="clickMenu">
-          <img src="/parts/hamburger-btn.svg" alt="Menu" />
+          <img src="@/assets/parts/hamburger-btn.svg" alt="Menu" />
         </button>
         <transition name="menu">
           <div class="menu" v-show="activeMenu">
@@ -30,7 +30,7 @@ const isCurrentPage = (path: string) => {
               <li>
                 <div>
                   <button class="close_btn" @click="clickMenu">
-                    <img src="/parts/close-btn.svg" alt="Menu" />
+                    <img src="@/assets/parts/close-btn.svg" alt="Menu" />
                   </button>
                 </div>
               </li>
