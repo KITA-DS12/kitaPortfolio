f2354126ec698fb41c3d5a46bcc54673a7523117
KITA-DS12 <kita.ds12@gmail.com>
2024-01-15 02:51:38 +0900

    add skill-page & background

---
+++ pages/skills.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <div class="content"></div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.container {
+  height: 80vh;
+  display: flex;
+  justify-content: center;
+
+  background-image: url("~/public/skill-background.png");
+  background-color: rgba(240, 240, 234, 0.8);
+  background-blend-mode: lighten;
+  background-size: 65vw;
+  background-repeat: no-repeat;
+  background-position: center;
+}
+</style>
