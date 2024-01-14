808d25fbb24c05f19c1adc015629190f8a1cbe12
KITA-DS12 <kita.ds12@gmail.com>
2024-01-15 06:53:25 +0900

    add work-in-progress and not-found

---
+++ components/WorkInProgress.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="content">
+    <img src="/work-in-progress.png" alt="Work in progress" />
+  </div>
+</template>
+<style lang="scss" scoped>
+.content {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+img {
+  width: 50%;
+}
+</style>
--- pages/blog.vue
+++ pages/blog.vue
@@ -1,3 +1,3 @@
 <template>
-  <NotFound />
+  <WorkInProgress />
 </template>
